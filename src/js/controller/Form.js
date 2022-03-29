f08831ef09542de2f16430d81e40c473894d8da4
ashwin-thinkify <ak@thinkify.io>
2022-03-29 15:38:34 +0530

    Removed unwanted code

--- src/js/controller/Form.js
+++ src/js/controller/Form.js
@@ -1,12 +1,6 @@
 import selectors from "../constants/selectors";
 import html from '../../htmls/main.html';
 
-function load(url, element) {
-  fetch(url).then((res) => {
-      console.log('res',res);
-    element.innerHTML = res;
-  });
-}
 export default class Form {
   constructor(id = "1") {
     this.id = id;
